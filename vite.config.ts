1e6e6722080b4059b1c78ceb178cb9545f72de28
AndrySaprigin <saprygin.2002@example.com>
2024-09-12 13:02:52 -0700

    have add port detection

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: parseInt(process.env.PORT as string) || 5173, // Используйте переменную окружения PORT
+    host: true
+  }
+});
